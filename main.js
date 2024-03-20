ad440e44f1567f4c1835697d8b83dd565b209cb8
= <=>
2024-03-20 16:40:05 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text", {
+    strings: ["Front End Developer", "Web Developer", "React Js Coder "],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
